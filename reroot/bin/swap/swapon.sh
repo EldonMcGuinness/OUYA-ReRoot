b078d6ccc13848e7de9e2e9bf367320564e91bc3
Eldon.McGuinness <Eldon.McGuinness@Outlook.com>
2015-02-02 23:46:23 -0500

    Properly waiting for USBDrive to mount swap
    Signed-off-by: Eldon.McGuinness <Eldon.McGuinness@Outlook.com>

--- reroot/bin/swap/swapon.sh
+++ reroot/bin/swap/swapon.sh
@@ -1,3 +1,33 @@
 #!/bin/sh
+# If no swapfile is found then continue
+# checking every 10 seconds for up to 15 minutes
+# Version 1.1
+
+LOGGER="/mnt/sdcard/swapon.log"
+echo "" > $LOGGER
+
+if [ ! -e "/mnt/usbdrive/swapfile1" ]; then
+    echo "SwapFile not found!" >> $LOGGER
+    LOOPCOUNT=0
+
+    while [ $LOOPCOUNT -lt 90 ]; do
+        sleep 10;
+
+        # Exit the loop if the drive is found
+        if [ -e "/mnt/usbdrive/swapfile1" ]; then
+            echo "SwapFile found!" >> $LOGGER
+            break;
+        fi
+
+        LOOPCOUNT=$((LOOPCOUNT + 1))
+    done
+
+    # If there is still no Android folder then exit
+    if [ ! -e "/mnt/usbdrive/swapfile1" ]; then
+        echo "SwapFile still not found, giving up" >> $LOGGER
+        exit
+    fi
+fi
+
 sysctl -w vm.swappiness=50
 swapon /mnt/usbdrive/swapfile1
